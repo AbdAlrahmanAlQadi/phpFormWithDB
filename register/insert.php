d1022c1f2571bbc473714ec48087b11741d9a1ab
Abd Alrahman AlQadi 🔻 <143936591+AbdAlrahmanAlQadi@users.noreply.github.com>
2024-07-10 14:00:47 +0300

    Update with Hashed Password

--- register/insert.php
+++ register/insert.php
@@ -6,8 +6,9 @@ $email = $_POST['email'];
 $phoneCode = $_POST['phoneCode'];
 $phone = $_POST['phone'];
 
-if (!empty($username) || !empty($password) || !empty($gender) || !empty($email) ||
- !empty($phoneCode) || !empty($phone)) {
+if (!empty($username) && !empty($password) && !empty($gender) && !empty($email) &&
+    !empty($phoneCode) && !empty($phone)) {
+    
     $host = "localhost";
     $dbUsername = "root";
     $dbPassword = "";
@@ -15,25 +16,25 @@ if (!empty($username) || !empty($password) || !empty($gender) || !empty($email) 
 
     $conn = new mysqli($host, $dbUsername, $dbPassword, $dbName);
 
-    if (mysqli_connect_error()) {
-        die('Connection Error ( '. mysqli_connect_error().')' . mysqli_connect_error());
-    } else  {
-        $SELECT = "SELECT email From register where email = ? limit 1";
-        $INSERT = "INSERT Into register (username, password, gender, email, phoneCode, phone) 
-        values (?,?,?,?,?,?)";
+    if ($conn->connect_error) {
+        die('Connection Error (' . $conn->connect_error . ')');
+    } else {
+        $SELECT = "SELECT email FROM register WHERE email = ? LIMIT 1";
+        $INSERT = "INSERT INTO register (username, password, gender, email, phoneCode, phone) VALUES (?, ?, ?, ?, ?, ?)";
 
         $stmt = $conn->prepare($SELECT);
         $stmt->bind_param("s", $email);
         $stmt->execute();
-        $stmt->bind_result($email);
         $stmt->store_result();
         $rnum = $stmt->num_rows;
 
         if ($rnum == 0) {
             $stmt->close();
 
+            $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
+
             $stmt = $conn->prepare($INSERT);
-            $stmt->bind_param("ssssii", $username, $password, $gender, $email, $phoneCode, $phone);
+            $stmt->bind_param("ssssii", $username, $hashedPassword, $gender, $email, $phoneCode, $phone);
             $stmt->execute();
             echo "Registration Successful";
         } else {
@@ -42,9 +43,8 @@ if (!empty($username) || !empty($password) || !empty($gender) || !empty($email) 
         $stmt->close();
         $conn->close();
     }
-
- } else {
+} else {
     echo "All fields are required.";
     die();
- }
+}
 ?>
